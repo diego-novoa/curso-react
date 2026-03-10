9f848d1dc7a28ddd255edd632ebfa7700e8af1df
Diego Novoa <diegonovoa1981@hotmail.com>
2026-03-10 11:56:22 -0500

    nuevo cambio

---
+++ Desktop/curso-react/Sección 4-Primeros-pasos-React/02-first-steps/src/1-componet/FirstStepsApp.tsx
@@ -0,0 +1,16 @@
+/*
+ejemplo basico componente
+*/
+export function FirstStepsApp() {
+  return (
+    <>
+      <h1>Hola Mundo !!</h1>
+      <p>Esto es un Parrafo</p>
+      <button>Clich me</button>
+
+      <div>
+        <h2>Hola dentro de un div</h2>
+      </div>
+    </>
+  );
+}
