ace76814b4c7910040a9d4ade6d3c4cf145afa4b
Diego Novoa <diegonovoa1981@hotmail.com>
2026-04-15 14:58:29 -0500

    actualizado

--- Desktop/curso-react/Sección 8 - Testing - Pruebas sobre GifsApp/03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ Desktop/curso-react/Sección 8 - Testing - Pruebas sobre GifsApp/03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,17 +1,16 @@
-import { useRef, useState } from "react";
-import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
-import type { Gif } from "../interfaces/gif.interface";
+import { useRef, useState } from 'react';
+import { getGifsByQuery } from '../actions/get-gifs-by-query.action';
+import type { Gif } from '../interfaces/gif.interface';
 
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
-
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const gifsCache = useRef<Record<string, Gif[]>>({});
 
   const handleTermClicked = async (term: string) => {
-
     if (gifsCache.current[term]) {
       setGifs(gifsCache.current[term]);
       return;
@@ -19,6 +18,7 @@ export const useGifs = () => {
 
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
+    gifsCache.current[term] = gifs;
   };
 
   const handleSearch = async (query: string = '') => {
@@ -34,16 +34,16 @@ export const useGifs = () => {
     setGifs(gifs);
 
     gifsCache.current[query] = gifs;
+    // console.log(gifsCache);
   };
 
   return {
-    // properties
+    // Properties
     gifs,
+    previousTerms,
 
-
-    // methods
+    // Methods
     handleSearch,
     handleTermClicked,
-    previousTerms,
   };
 };
